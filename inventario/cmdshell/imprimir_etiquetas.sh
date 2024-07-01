1f6a97cbc0eb1a401939324740244a20ee27f118
SEBASTIAN CATIVA TOLOSA <cativa@cnea.gov.ar>
2024-07-01 15:49:07 -0300

    inventario -> salvaguardia

--- inventario/cmdshell/imprimir_etiquetas.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Impresión de todos los archivos de la carpeta $PWD"
-cd ../out
-for file in *.zpl; do lp -d zebra-raw <<< "$(cat "$file")"; done
-cd ..
-read -n 1 -s -r -p "Presione cualquier tecla para finalizar"
-echo " "
-
