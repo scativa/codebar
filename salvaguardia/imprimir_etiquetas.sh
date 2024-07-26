1dfc7dec8c5487da9053bafc091d5394fbb6e4ad
SEBASTIAN CATIVA TOLOSA <--local>
2024-07-26 16:04:22 -0300

    script seleccionar+imprimir linux

--- salvaguardia/imprimir_etiquetas.sh
+++ salvaguardia/imprimir_etiquetas.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
-carpeta=./etiquetas/
+
+# Verificar si se ha pasado un parámetro
+if [ $# -eq 0 ]; then
+    echo "No se ha proporcionado ninguna carpeta como parámetro. Por defecto se utilzará la carpeta "${carpeta}""
+	carpeta=./etiquetas/
+else
+	# Obtener el nombre de la carpeta del primer parámetro
+	carpeta="$1"
+fi
+
+
+# Verificar si la carpeta existe
+if [ -d "$carpeta" ]; then
+    echo "La carpeta seleccionada es: $carpeta"
+else
+    echo "La carpeta '$carpeta' no existe."
+    exit 1
+fi
 
 echo "Impresión de todos los archivos de la carpeta $PWD/"${carpeta}""
 for file in "${carpeta}"/*.zpl; do lp -d zebra-raw <<< "$(cat "$file")"; done
